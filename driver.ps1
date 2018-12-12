88b1ad11b475322e8bf2cf78f26363bcbb1f3446
SN MADDULA <snmaddula@outlook.com>
2018-12-12 15:08:20 +0530

    Update driver.ps1

--- driver.ps1
+++ driver.ps1
@@ -1,11 +1,13 @@
 ############################################
 ### Powershell based Ansible & AWX setup ###
 ############################################
+cd ~
+mkdir __lab__ && cd __lab__
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.20.0.windows.1/Git-2.20.0-64-bit.exe -OutFile gitbin.exe
 Invoke-WebRequest -Uri https://releases.hashicorp.com/vagrant/2.2.2/vagrant_2.2.2_x86_64.msi -OutFile vag.msi
-cd ~
-mkdir __lab__ && cd __lab__
+
 cmd "/C .\gitbit.exe"
 rm gitbin.exe
 cmd "/C .\vag.msi"
