5419fe7a06f9f9ddc42f4c768864dfd7607d289b
SN MADDULA <snmaddula@outlook.com>
2018-12-12 14:58:51 +0530

    Update driver.ps1

--- driver.ps1
+++ driver.ps1
@@ -4,9 +4,13 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.20.0.windows.1/Git-2.20.0-64-bit.exe -OutFile gitini.exe
 Invoke-WebRequest -Uri https://releases.hashicorp.com/vagrant/2.2.2/vagrant_2.2.2_x86_64.msi -OutFile vag.msi
-
+cd ~
+mkdir __lab__ && cd __lab__
 cmd "/C .\gitbit.exe"
+rm gitbit.exe
 cmd "/C .\vag.msi"
+rm vag.msi
+cd ..
 cmd "/C git clone https://github.com/snmaddula/ansible-magic-lab.git"
 cd ansible-magic-lab
 cmd "/c vagrant up"
