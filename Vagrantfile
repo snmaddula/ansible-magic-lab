eac29aebec6af2f9aff38cde67347836119d5bf5
snmaddula <sn_maddula@outlook.com>
2018-12-12 12:13:03 +0530

    v1 done!

--- Vagrantfile
+++ Vagrantfile
@@ -2,15 +2,15 @@ Vagrant.configure("2") do |config|
   config.vm.box = "snmaddula/ubuntu"
   config.vm.box_version = "14.04"
   config.vm.box_check_update = false
-  config.vm.network "private_network", ip: "192.168.33.10
+  config.vm.network "private_network", ip: "192.168.33.10"
+  
   config.vm.provider "virtualbox" do |vb|
-    vb.memory = "1024"
+    vb.memory = "4096"
   end
   
   config.vm.provision "shell", inline: <<-SHELL
     git clone https://github.com/snmaddula/ansible-magic-lab.git magic-lab
-	cd magic-lab
-	chmod +x docker-compose
+	cd magic-lab && chmod +x docker-compose
 	./docker-compose up -d
   SHELL
 end
---
+++ driver.ps1
@@ -0,0 +1,14 @@
+############################################
+### Powershell based Ansible & AWX setup ###
+############################################
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.20.0.windows.1/Git-2.20.0-64-bit.exe -OutFile gitini.exe
+Invoke-WebRequest -Uri https://releases.hashicorp.com/vagrant/2.2.2/vagrant_2.2.2_x86_64.msi -OutFile vag.msi
+
+cmd "/C .\gitbit.exe"
+cmd "/C .\vag.msi"
+cmd "/C git clone https://github.com/snmaddula/ansible-magic-lab.git"
+cd ansible-magic-lab
+cmd "/c vagrant up"
+
+
