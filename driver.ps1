310948dd7ed9cdb3461e34586875278c1ecabb87
SN MADDULA <snmaddula@outlook.com>
2018-12-12 19:59:52 +0530

    Update driver.ps1

--- driver.ps1
+++ driver.ps1
@@ -7,7 +7,7 @@ mkdir __lab__ && cd __lab__
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.20.0.windows.1/Git-2.20.0-64-bit.exe -OutFile gitbin.exe
-cmd "/C .\gitbit.exe /SP- /SILENT /NOCANCEL /NORESTART /SAVEINF=silent_git"
+cmd "/C .\gitbin.exe /SP- /SILENT /NOCANCEL /NORESTART /SAVEINF=silent_git"
 $env:Path="$env:Path;C:\Program Files\Git\bin\;C:\Program Files\Git\usr\bin\;"
 rm gitbin.exe
 
